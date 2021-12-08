1f148b80a44a29f6aa1652e13e977e59a61526a7
G4L4XYDR01D <1qaz2wsx@gmail.com>
2021-12-08 15:47:29 +0100

    Poprawka do bazy danych z tabelą z użytkownikami z dodaną kolumną administrator

--- projekt.sql
+++ projekt.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 06 Gru 2021, 23:13
+-- Czas generowania: 08 Gru 2021, 15:42
 -- Wersja serwera: 10.4.22-MariaDB
 -- Wersja PHP: 8.0.13
 
@@ -33,24 +33,25 @@ CREATE TABLE `uzytkownicy` (
   `nazwisko` varchar(15) DEFAULT NULL,
   `email` varchar(30) DEFAULT NULL,
   `login` varchar(30) DEFAULT NULL,
-  `haslo` varchar(20) DEFAULT NULL
+  `haslo` varchar(20) DEFAULT NULL,
+  `administrator` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Zrzut danych tabeli `uzytkownicy`
 --
 
-INSERT INTO `uzytkownicy` (`id`, `imie`, `nazwisko`, `email`, `login`, `haslo`) VALUES
-(1, 'Grzegorz', 'Kowalski', 'grze326@interia.pl', 'grzegorzk', 'gjd83kf9'),
-(2, 'Alicja', 'Kwiatkowska', 'alikwi613@wp.pl', 'kwiatk135', 'gj48fj39d'),
-(3, 'Robert', 'Nowak', 'nowro533@onet.pl', 'nowak12351', 'f34h893'),
-(4, 'Gabriela', 'Piotrkowska', 'piotra32@gmail.com', 'gabipiotr1314', '3c525vb5'),
-(5, 'Ewelina', 'Korzeniowska', 'korze533@wp.pl', 'korz515', 'gv34mi9'),
-(6, 'Damian', 'Zielinski', 'ziela254@gmail.com', 'zieldam2341', '54ybwwb46'),
-(7, 'Karolina', 'Pocztowska', 'karpocz131@interia.pl', 'poczt4144', '523b5v35'),
-(8, 'Kamil', 'Michalski', 'michal43@o2.pl', 'michkam413', 'gbj903ge54y'),
-(9, 'Justyna', 'Jaworska', 'jawor32@onet.pl', 'justynaj21', 'f344v6364'),
-(10, 'Emil', 'Malinowski', 'malina52@gmail.com', 'emilmal51', 'v535b533');
+INSERT INTO `uzytkownicy` (`id`, `imie`, `nazwisko`, `email`, `login`, `haslo`, `administrator`) VALUES
+(1, 'Grzegorz', 'Kowalski', 'grze326@interia.pl', 'grzegorzk', 'gjd83kf9', 0),
+(2, 'Alicja', 'Kwiatkowska', 'alikwi613@wp.pl', 'kwiatk135', 'gj48fj39d', 0),
+(3, 'Robert', 'Nowak', 'nowro533@onet.pl', 'nowak12351', 'f34h893', 1),
+(4, 'Gabriela', 'Piotrkowska', 'piotra32@gmail.com', 'gabipiotr1314', '3c525vb5', 0),
+(5, 'Ewelina', 'Korzeniowska', 'korze533@wp.pl', 'korz515', 'gv34mi9', 0),
+(6, 'Damian', 'Zielinski', 'ziela254@gmail.com', 'zieldam2341', '54ybwwb46', 0),
+(7, 'Karolina', 'Pocztowska', 'karpocz131@interia.pl', 'poczt4144', '523b5v35', 0),
+(8, 'Kamil', 'Michalski', 'michal43@o2.pl', 'michkam413', 'gbj903ge54y', 0),
+(9, 'Justyna', 'Jaworska', 'jawor32@onet.pl', 'justynaj21', 'f344v6364', 0),
+(10, 'Emil', 'Malinowski', 'malina52@gmail.com', 'emilmal51', 'v535b533', 0);
 
 --
 -- Indeksy dla zrzutów tabel
