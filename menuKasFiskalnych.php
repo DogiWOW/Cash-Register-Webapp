f86ba8cbe320db9a387a9fa1b136cfba08db21d6
Mateusz Jankowski <Mati.Jankowski123@wp.pl>
2021-12-11 20:02:56 +0100

    Dodano layout menuKasFiskalnych.php

--- menuKasFiskalnych.php
+++ menuKasFiskalnych.php
@@ -1,29 +1,39 @@
 <?php
  session_start();
- 
 ?>
-
 <!DOCTYPE html>
 <html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/style-menu.css">
     <title>Menu Kas Fiskalnych</title>
 </head>
 <body>
-    <h1>System Zarządzania Kasami Fiskalnymi</h1>
-    <a href='logout.php'>Wyloguj</a><br>
-    <a href='logout.php'>Lista kas fiskalnych</a><br>
-    <a href='logout.php'>Dodaj kasę fisklaną</a><br>
-    <a href='logout.php'>Klienci</a><br>
-    <?php
-    if($_SESSION['admin'] == 1){
-        echo "<a href='logout.php'>Użytkownicy (administrator)</a><br>";
-    }
-    ?>
-    <a href='logout.php'>Przeglądy</a><br>
-    <a href='logout.php'>Dodaj przegląd</a><br>
-    <a href='logout.php'>Wyślij maila do klienta</a><br>
+    <div id='main'>
+        <div id="heading_user">
+            <div id="heading">
+                <h1>System Zarządzania Kasami Fiskalnymi</h1>
+            </div>
+            <div id='logged_user'>
+                Zalogowano jako: <?php echo $_SESSION['imie']; ?>
+                <a href='logout.php'>Wyloguj</a>
+            </div>
+        </div>
+        <div class='anchor_buttons'>
+            <button><a href='logout.php'>Lista kas fiskalnych</a></button>
+            <button><a href='logout.php'>Dodaj kasę fisklaną</a></button>
+            <button><a href='logout.php'>Klienci</a></button>
+            <button><a href='logout.php'>Przeglądy</a></button>
+            <button><a href='logout.php'>Dodaj przegląd</a></button>
+            <button><a href='logout.php'>Wyślij maila do klienta</a></button>
+            <?php 
+            if($_SESSION['admin'] == 1){
+                echo "<button><a href='logout.php'>Użytkownicy (administrator)</a></button>";
+            }
+            ?>
+        </div>
+    </div>
 </body>
 </html>
